1615d028ac9f53bd9e6d4a1c3231a4bd97dba868
songqing sun <sunsq1991@gmail.com>
2020-06-18 21:20:31 -0400

    first draft

---
+++ src/components/LeftMenuFooter/index.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * LeftMenuFooter
+ *
+ */
+
+import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { PropTypes } from 'prop-types';
+
+import Wrapper from './Wrapper';
+import messages from './messages.json';
+
+defineMessages(messages);
+
+function LeftMenuFooter({ version }) {
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <FormattedMessage
+          id={messages.poweredBy.id}
+          defaultMessage={messages.poweredBy.defaultMessage}
+          key="poweredBy"
+        />
+        <a key="website" href="https://moocads.com" target="_blank" rel="noopener noreferrer">
+          Mooc Advertising Inc.
+        </a>
+      </div>
+    </Wrapper>
+  );
+}
+
+LeftMenuFooter.propTypes = {
+  version: PropTypes.string.isRequired,
+};
+
+export default LeftMenuFooter;
