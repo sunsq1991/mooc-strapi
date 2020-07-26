ec076bc5e2900f5e92fd78c0c46ca9eb8c5d9226
songqing sun <sunsq1991@gmail.com>
2020-07-26 01:17:50 -0400

    read database url directly incase of db url change

--- mooc-strapi-resources/config/env/production/database.js
+++ mooc-strapi-resources/config/env/production/database.js
@@ -1,19 +1,24 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
-      },
-      options: {
-        ssl: false,
+const {ConnectionString} = require('connection-string');
+
+module.exports = ({ env }) => {
+  const db = new ConnectionString(env('DATABASE_URL', 'postgres://songqingsun@localhost:5432/test'));
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: db.hosts[0].name,
+          port: db.hosts[0].port,
+          database: db.path[0],
+          username: db.user,
+          password: db.password,
+        },
+        options: {
+          ssl: false,
+        },
       },
     },
-  },
-});
+  };
+}
